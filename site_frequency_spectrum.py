c64a2df13ceb0c76105f9b9879fc147448ff00d5
roger893451 <roger893451@users.noreply.github.com>
2021-06-03 18:22:35 -0500

    cleanup
    Clean up redundant files

--- site_frequency_spectrum.py
+++ site_frequency_spectrum.py
@@ -3,26 +3,21 @@
 import sys
 import re
 import pandas as pd
+from subprocess import Popen, PIPE
 
-#with open(sys.argv[1],'r') as a:
-#    sum={}
-#    for i in a:
-#        if re("#",i):
-#            pass
-#        else:
-#            i=i.strip()
-#            i_list=i_list.split()
 a=pd.read_table(sys.argv[1], header=0, dtype={"#CHROM": "string"})
-#out=open(sys.argv[1].split(".")[0]+"out.txt","w")
+annoted_a=sys.argv[2]
+
+Popen(['sh','prepare_non_synonymous.sh',sys.argv[1], sys.argv[2]],shell=False, stderr=PIPE, stdout=PIPE)
+
+#a=pd.read_table('all_exome_non_cancer_af_nonsynonymous_nononsense_uniq.bed', header=0, dtype={"#CHROM": "string"})
 out=sys.argv[1].split(".")[0]+"_out.txt"
 print(a.describe())
 a.drop_duplicates(keep="first",inplace=True)
 b=a.iloc[:,6].value_counts()
-#b=a["non_cancer_AC_alt"].value_counts()
 c=b.sum()
 print("sum=",c)
 b = pd.DataFrame(b).reset_index()
-#print(a.describe())
 b=b.drop([0])
 c=b.sum()
 print("exclude 0 sum=",c)
